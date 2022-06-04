9e7810cd58fa134617941c31515080613e8ab3ef
manuandru <manuandru987@gmail.com>
2022-06-04 14:41:11 +0200

    Initial commit

---
+++ Sushime/SushimeApp.swift
@@ -0,0 +1,20 @@
+//
+//  SushimeApp.swift
+//  Sushime
+//
+//  Created by Manuel Andruccioli on 04/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SushimeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
