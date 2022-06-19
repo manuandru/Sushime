1dff525c72158288816a9495d9a4628506d50f6b
manuandru <manuandru987@gmail.com>
2022-06-19 19:31:13 +0200

    add icon set

---
+++ Sushime/View/Room/SelectionButton.swift
@@ -0,0 +1,20 @@
+//
+//  SelectionButton.swift
+//  Sushime
+//
+//  Created by Manuel Andruccioli on 19/06/22.
+//
+
+import SwiftUI
+
+struct SelectionButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectionButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectionButton()
+    }
+}
