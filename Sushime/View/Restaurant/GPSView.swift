3c8c576aa1c3173652d2391a708bf6a15f98b540
manuandru <manuandru987@gmail.com>
2022-06-22 19:53:49 +0200

    add new view for gps

---
+++ Sushime/View/Restaurant/GPSView.swift
@@ -0,0 +1,20 @@
+//
+//  GPSView.swift
+//  Sushime
+//
+//  Created by Manuel Andruccioli on 22/06/22.
+//
+
+import SwiftUI
+
+struct GPSView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GPSView_Previews: PreviewProvider {
+    static var previews: some View {
+        GPSView()
+    }
+}
